4394572b70502fd7f7dad87f35cf0d86a66eac04
eman <emannasr2001@gmail.com>
2024-10-11 19:23:40 +0200

    update the validation

--- Routes/excludedRoutes.js
+++ Routes/excludedRoutes.js
@@ -5,7 +5,7 @@ const verifytoken=require('../middleware/verifytoken');
 const { excludedValidationRules, validateExcluded } = require('../middleware/excluded.validation');
 
 router.get('/api/excluded/getallexcludeds',verifytoken,excludedcontroller.getallexcluded);
-router.post('/api/excluded/addexcluded',verifytoken, excludedValidationRules(), validateExcluded,excludedcontroller.addexcluded);
+router.post('/api/excluded/addexcluded',verifytoken,excludedcontroller.addexcluded);
 router.patch('/api/excluded/updateexcluded/:excludedId',verifytoken, excludedValidationRules(),validateExcluded,excludedcontroller.updateExcluded);
 router.delete('/api/excluded/deleteexcluded/:excludedId',verifytoken,excludedcontroller.deleteExcluded);
 
