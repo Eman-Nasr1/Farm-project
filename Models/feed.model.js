f42f61d387d607acab36abd8e8a3c5b71edfe6dc
eman <emannasr2001@gmail.com>
2024-12-07 18:00:55 +0200

    add feed and feed by location shed

--- Models/feed.model.js
+++ Models/feed.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../Models/user.model');
+const User = require('./user.model');
 const Feedschema = new mongoose.Schema({
     name: {
         type: String,
