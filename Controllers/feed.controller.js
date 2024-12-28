a38f304b6c02b95156d49eb10e754f68fd5acdf2
eman <emannasr2001@gmail.com>
2024-12-28 20:47:36 +0200

    handle the update function of feed by shed

--- Controllers/feed.controller.js
+++ Controllers/feed.controller.js
@@ -168,13 +168,13 @@ const addFeedToShed = asyncwrapper(async (req, res, next) => {
 
 const updateFeedToShed = asyncwrapper(async (req, res, next) => {
     const userId = req.userId; // Get the user ID from the token
-    const shedEntryId = req.params.shedEntryId; // ID of the ShedEntry to update
+    const shedEntryId = req.params.feedShedId; // ID of the ShedEntry to update
     const updatedData = req.body; // Data to update
 
     // Find the existing shed entry document
     
-    let shedEntry = await ShedEntry.findOne({ _id: shedEntryId, owner: userId });
-    console.log(shedEntryId,updatedData,shedEntry);
+    let shedEntry = await ShedEntry.findOne({ _id: feedShedId, owner: userId });
+    //console.log(shedEntryId,updatedData,shedEntry);
     if (!shedEntry) {
         const error = AppError.create('Shed entry not found or unauthorized to update', 404, httpstatustext.FAIL);
         return next(error);
