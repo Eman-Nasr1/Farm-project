60b7f51f8d8b07d23959e1558eed8dc16cc81708
eman <emannasr2001@gmail.com>
2025-01-19 23:33:51 +0200

    update feed

--- Models/shedFeed.model.js
+++ Models/shedFeed.model.js
@@ -1,35 +1,36 @@
-const mongoose = require('mongoose');  
-const Feed = require('../Models/feed.model');  
-const User = require('../Models/user.model');
+const mongoose = require("mongoose");
 
-const ShedFeedSchema = new mongoose.Schema({  
-    feed: {  
-        type: mongoose.Schema.Types.ObjectId,  
-        ref: 'Feed',  
-        required: true  
-    },  
-    locationShed: {  
-        type: String,  
-        required: true  
-    },  
-    quantity: {  
-        type: Number,  
-        required: true  
-    },  
-    date: {  
-        type: Date,  
-        default: Date.now,  
-        required: true  
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-       
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-});  
+const feedSchema = new mongoose.Schema({
+  feedName: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('ShedEntry', ShedFeedSchema);
+const shedEntrySchema = new mongoose.Schema({
+  locationShed: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  feeds: {
+    type: [feedSchema],
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+}, { timestamps: true });
+
+const ShedEntry = mongoose.model("ShedEntry", shedEntrySchema);
+
+module.exports = ShedEntry;
