17a7d28bbcfd367e13ec30c77060fdd936ca5d32
eman <emannasr2001@gmail.com>
2025-02-02 00:48:34 +0200

    handle the pagination

--- Routes/treatmentRoutes.js
+++ Routes/treatmentRoutes.js
@@ -4,6 +4,9 @@ const treatmentController=require('../Controllers/treatment.controller');
 const verifytoken=require('../middleware/verifytoken');
 
 router.get('/api/treatment/getalltreatmentes',verifytoken,treatmentController.getallTreatments);
+
+router.get('/api/treatment/gettreatments',verifytoken,treatmentController.getTreatments);
+
 router.get('/api/treatment/getsingletreatment/:treatmentId',verifytoken,treatmentController.getsnigleTreatment);
 router.post('/api/treatment/addtreatment',verifytoken,treatmentController.addTreatment);
 router.patch('/api/treatment/updatetreatment/:treatmentId',verifytoken,treatmentController.updateTreatment);
