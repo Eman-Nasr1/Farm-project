d3c31013af39f91afe368c9cdc843ab090318557
Eman-Nasr1 <eman.n@anasacademy.uk>
2025-09-06 13:39:25 +0200

    add get single in route

--- Routes/locationshedRoutes.js
+++ Routes/locationshedRoutes.js
@@ -5,7 +5,7 @@ const locationShedController=require('../Controllers/locationshed.controller');
 
 router.get('/api/location/GetAll-Locationsheds',verifytoken,locationShedController.getAllLocationSheds);
 router.get('/api/location/GetAll-Locationsheds-menue',verifytoken,locationShedController.getAllLocationShedsWithoutPagination);
-router.get('/api/location/GetSingle-Locationshed',verifytoken,locationShedController.getSingleLocationShed);
+router.get('/api/location/GetSingle-Locationshed/:locationShedId',verifytoken,locationShedController.getSingleLocationShed);
 router.post('/api/location/addlocationshed',verifytoken,locationShedController.addLocationShed);
 router.patch('/api/location/updatelocationShed/:locationShedId',verifytoken, locationShedController.updateLocationShed);
 router.delete('/api/location/deletelocationShed/:locationShedId',verifytoken,locationShedController.deleteLocationShed);
